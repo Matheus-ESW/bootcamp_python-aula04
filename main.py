7ea79e329ee62c0f5ae72b33e164d4f464d9e6f3
Matheus Ramos <matheussoaresramos@gmail.com>
2025-10-30 20:25:34 -0300

    Finalizado exercicio 11

--- main.py
+++ main.py
@@ -112,7 +112,7 @@
 #     {"nome": "Bob", "idade": 25},
 #     {"nome": "Carol", "idade": 20}
 # ]
-# pessoas.sort(key=lambda pessoa: pessoa["nome"])
+# pessoas.sort(key=lambda pessoa: Wpessoa["nome"])
 
 # print(pessoas)
 
@@ -127,9 +127,26 @@
 ## 10. Divisão de Dados em Grupos
 ## Objetivo: Dada uma lista de valores, dividir em duas listas: uma para valores pares e outra para ímpares.
 
-valores = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-pares = [valor for valor in valores if valor % 2 == 0]
-impares = [valor for valor in valores if valor % 2 != 0]
+# valores = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# pares = [valor for valor in valores if valor % 2 == 0]
+# impares = [valor for valor in valores if valor % 2 != 0]
 
-print("Pares:", pares)
-print("Ímpares:", impares)
+# print("Pares:", pares)
+# print("Ímpares:", impares)
+
+## Exercícios com Dicionários
+# 11. Atualização de Dados
+# Objetivo: Dada uma lista de dicionários representando produtos, atualizar o preço de um produto específico.
+
+produtos = [
+    {"id": 1, "nome": "Teclado", "preço": 100},
+    {"id": 2, "nome": "Mouse", "preço": 80},
+    {"id": 3, "nome": "Monitor", "preço": 300}
+]
+
+# Atualizar o preço do produto com id 2 para 90
+for produto in produtos:
+    if produto["id"] == 2:
+        produto["preço"] = 90
+
+print(produtos)
